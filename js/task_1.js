b1c9ec25173e97b50cbb7d5aeef271b5fec75af6
Maxim <Tor968@yahoo.com>
2020-09-16 16:05:50 +0300

    Update task_1.js

---
+++ js/task_1.js
@@ -0,0 +1,22 @@
+// const magicBtn = document.querySelector('.js-magic-btn');
+
+/*
+ * document.querySelector(selector) и document.querySelectorAll(selector)
+ *   selector - любой валидный CSS-селектор
+ *
+ * Что возвращают?
+ */
+
+// const navEl = document.querySelector('.site-nav');
+// console.log('navEl', navEl);
+
+// const navLinksEl = document.querySelectorAll('.site-nav__link');
+// console.log('navLinksEl', navLinksEl);
+
+/*
+ * Document.querySelector* и Element.querySelector*
+ */
+const listEl = document.querySelectorAll('.item h2');
+console.log(listEl);
+// const headingList = listEl.querySelector('h2');
+// console.log(headingList);
