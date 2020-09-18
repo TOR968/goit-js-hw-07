e3ddb4c876aab6a8413623fb90de8bb24cef3346
Maxim <Tor968@yahoo.com>
2020-09-18 10:31:04 +0300

    Update task_2.js

--- js/task_2.js
+++ js/task_2.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+for (let ingredient of ingredients) {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  console.log(itemEl);
+  const ul = document.querySelector('#ingredients');
+
+  ul.appendChild(itemEl);
+}
