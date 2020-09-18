c63170e202b40985b1ee51ec303f9fd6bbbd167e
Maxim <Tor968@yahoo.com>
2020-09-18 09:27:41 +0300

    task_1

--- js/task_1.js
+++ js/task_1.js
@@ -1,16 +1,13 @@
+const ul = Array.from(document.querySelector('#categories').children);
+for (let elem of ul) {
+  let category = elem.firstElementChild.textContent;
+  let quantityElem = elem.lastElementChild.children.length;
+  console.log(`Категория: ${category} \nКоличество элементов: ${quantityElem}`);
+}
+
 // const items = document.querySelectorAll('.item');
 // Array.prototype.forEach.call(items, element => {
 //   const title = element.querySelector('h2').innerHTML;
 //   const itemsLength = element.querySelectorAll('li').length;
-//   console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
+//   console.log(`Категория: ${title} \n Количество элементов: ${itemsLength}`);
 // });
-
-// const ul = Array.from(document.querySelector('#categories').children);
-// for (let elem of ul) {
-//   let category = elem.firstElementChild.textContent;
-//   let quantityElem = elem.lastElementChild.children.length;
-//   console.log(`Категория: ${category} \nКоличество элементов: ${quantityElem}`);
-// }
-
-// const listEl = document.querySelectorAll('.item');
-// console.log(listEl);
