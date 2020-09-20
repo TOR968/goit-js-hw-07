dd8800005b212a591525fcffd5c62b77cf57f943
Maxim Turbal <tor968@yahoo.com>
2020-09-20 15:02:33 +0300

    Update task_6.js

--- js/task_6.js
+++ js/task_6.js
@@ -2,24 +2,14 @@ const input = document.querySelector('#validation-input');
 const textLength = input.getAttribute('data-length');
 input.addEventListener('blur', onInputBlur);
 
-function onInputBlur() {
-  console.log(input.value.length);
-  if (textLength > input.value.length) {
-    input.classList.remove('valid');
-    input.classList.add('invalid');
-  } else {
-    input.classList.remove('invalid');
+function onInputBlur(event) {
+  if (event.currentTarget.value.length === Number(textLength)) {
     input.classList.add('valid');
+    input.classList.remove('invalid');
+  } else {
+    input.classList.add('invalid');
+    input.classList.remove('valid');
   }
-}
 
-// document.getElementById('validation-input').onblur = function () {
-//   console.log(this.value.length);
-//   if (this.getAttribute('data-length') > this.value.length) {
-//     this.classList.remove('valid');
-//     this.classList.add('invalid');
-//   } else {
-//     this.classList.remove('invalid');
-//     this.classList.add('valid');
-//   }
-// };
+  console.log('event=', event.currentTarget.value.length);
+}
